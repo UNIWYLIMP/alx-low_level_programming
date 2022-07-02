2e104c50279262584bb362cc3de629e17e492651
UNIWYLIMP <77296707+UNIWYLIMP@users.noreply.github.com>
2022-07-02 03:11:15 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,11 +12,8 @@ int main(void)
 
 	for (d = 0; d < 90; d++)
 	{
-		for ()
-		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
 		if (d != 89)
 		{
 			putchar(',');
