9b3b717aa34cec24af99cefc5223873df67f124d
UNIWYLIMP <77296707+UNIWYLIMP@users.noreply.github.com>
2022-07-02 02:35:01 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,10 @@ int main(void)
 char low;
 
 for (low = 'a'; low <= 'z'; low++)
-    putchar(low);
+    {putchar(low);}
 for (low = 'A'; low <= 'Z'; low++)
-	putchar(low);
-	putchar('\n');
+	{putchar(low);
+	putchar('\n');}
 
 return (0);
 }
