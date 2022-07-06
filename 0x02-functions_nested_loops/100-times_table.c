d2de2d826d1b769cc28f418c7cde0da45fe4b3fa
UNIWYLIMP <77296707+UNIWYLIMP@users.noreply.github.com>
2022-07-06 01:55:15 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,43 @@
 #include "main.h"
 /**
- * print_times_table - Entry point
- * @n: input
- * Return: Always 0 (Success)
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
  */
 void print_times_table(int n)
 {
+	int rone, cone, d;
+        
+        if (n<0 || n>15)
+        {
+                return (0)
+        }
+	for (rone = 0; rone <= n; rone++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
+		{
+			d = (rone * cone);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
 
-	return (0);
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
 }
