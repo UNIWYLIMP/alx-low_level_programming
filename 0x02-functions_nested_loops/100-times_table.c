00a4a669850de59f8412a424b5976a23e20b98f8
UNIWYLIMP <77296707+UNIWYLIMP@users.noreply.github.com>
2022-07-06 05:12:14 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,60 +1,44 @@
 #include "main.h"
+
 /**
- * times_table -  a function that prints the 9 times table, starting with 0
- * rone = row, cone = column, d = digits of current result
- * Return: times table
- * add extra space past single digit
- */
+* print_times_table - Prints the times table of the input,
+* starting with 0.
+* @n: integer variable
+*/
 void print_times_table(int n)
 {
-	int rone, cone, d;
-        
-        if (n<0 || n>15)
-        {
-                
-        }
-        else
-        {
-	for (rone = 0; rone <= n; rone++)
-	{
-		_putchar('0');
-        if(n>0)
-                {_putchar(',');
-		_putchar(' ');
-                _putchar(' ');
-                _putchar(' '); }
-		for (cone = 1; cone <= n; cone++)
-		{
-			d = (rone * cone);
+int i, j, result;
 
-			if ((d / 10) > 0 && d < 100)
-			{
-                                _putchar(' ');
-				_putchar((d / 10) + '0');
-			}
-                        else if ((d / 10) > 0 && d >= 100)
-                        { 
-                                _putchar(' ');
-                                _putchar((d / 100) + '0');
-                                _putchar(((d % 100) / 10) + '0');
-                        }
-			else
-			{
-				
-			}
-			_putchar((d % 10) + '0');
-   
-			if (cone < n)
-			{
-				_putchar(',');
-				_putchar(' ');
-                        if (d < 99)        
-                                {_putchar(' '); }
-                        }
-		}
-		_putchar('\n');
-        }
+if (n >= 0 && n <= 15)
+{
+for (i = 0; i <= n; i++)
+{
+_putchar('0');
 
-        return;
+for (j = 1; j <= n; j++)
+{
+_putchar(',');
+_putchar(' ');
 
+result = i * j;
+
+if (result <= 99)
+_putchar(' ');
+if (result <= 9)
+_putchar(' ');
+
+if (result >= 100)
+{
+_putchar((result / 100) + '0');
+_putchar(((result / 10)) % 10 + '0');
+}
+else if (result <= 99 && result >= 10)
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
+}
 }
