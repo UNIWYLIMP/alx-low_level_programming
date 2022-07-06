fb0d226efd5b69f8766a458c794b36005c7126d7
UNIWYLIMP <77296707+UNIWYLIMP@users.noreply.github.com>
2022-07-06 04:03:02 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,6 +8,16 @@
  */
 int main(void)
 {
-
+        int super = 1;
+        int lobal = 0;
+        while (super < 1024)
+        {
+         if ((super%3) == 0 || (super%5) == 0)
+         {
+                 lobal += super;
+         }
+         super++;
+        }
+        printf(lobal);
 	return (0);
 }
