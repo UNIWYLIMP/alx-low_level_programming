f2e6e8d7254aef78316fa241961cb74b4770505a
UNIWYLIMP <77296707+UNIWYLIMP@users.noreply.github.com>
2022-07-06 02:47:09 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,12 +26,8 @@ void print_times_table(int n)
 		{
                         _putchar(' ');
 			d = (rone * cone);
-                        if ((d / 100) > 0)
-			{
-				_putchar(d);
-			}
 
-			else if ((d / 10) > 0)
+			if ((d / 10) > 0)
 			{
 				_putchar((d / 10) + '0');
 			}
