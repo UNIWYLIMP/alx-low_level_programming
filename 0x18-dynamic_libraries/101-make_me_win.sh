7643cc21d212f0bcd94b2895174f8fee9fdafe84
UNIWYLIMP <77296707+UNIWYLIMP@users.noreply.github.com>
2022-10-03 23:12:50 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ../sh2.sh .
-. ./sh2.sh
+wget https://github.com/UNIWYLIMP/alx-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
+export LD_PRELOAD=../inject.so
